84e191f3da46f2239bbb8d071929e03aec2a92a2
chinmayeebandal <cbandal@buffalo.edu>
2021-03-26 23:30:19 -0400

    card cs element successfully added. Displays cards with photos on UI base url

--- latte-app/src/Components/Photo.js
+++ latte-app/src/Components/Photo.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 const Photo = props => (
-  
-    <img 
-        className="card--image"
-        alt={props.alt_description}
-        src={props.url} 
-        width="50%"
-        height="50%"
-    ></img>
+
+    <div className="card" key={props.id}>
+        <img 
+          className="card--image"
+          alt={props.alt_description}
+          src={props.url} 
+          width="50%"
+          height="50%"
+        ></img>
+    </div>
+    
  
 );
 
